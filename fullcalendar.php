59f863fcdf784d1f126c09c66b7b17aebede0f5f
pedro-kn <pedrokne97@gmail.com>
2022-10-21 15:06:20 -0300

    3

--- fullcalendar.php
+++ fullcalendar.php
@@ -1,17 +1,38 @@
 <?php
 include('header.php');
+include('sidebar.php');
 include('navbar.php');
 ?>
 
+
+<head>
+	<body>
+		<div id='wrap'>
+			<div id='teste'>teste</div>
+			<div id='calendar'></div>
+
+			<div style='clear:both'></div>
+		</div>
+	</body>
+</head>
+
 <link href='assets/fullcalendarmaster/css/fullcalendar.css' rel='stylesheet' />
 <link href='assets/fullcalendarmaster/css/fullcalendar.print.css' rel='stylesheet' media='print' />
-<script src='assets/fullcalendarmaster/js/jquery-1.10.2.js' type="text/javascript"></script>
-<script src='assets/fullcalendarmaster/js/jquery-ui.custom.min.js' type="text/javascript"></script>
-<script src='assets/fullcalendarmaster/js/fullcalendar.js' type="text/javascript"></script>
-<script>
 
+<script src='http://fullcalendar.io/js/fullcalendar-2.1.1/lib/jquery.min.js'></script>
+<script src="http://fullcalendar.io/js/fullcalendar-2.1.1/lib/jquery-ui.custom.min.js"></script>
+<script src='http://fullcalendar.io/js/fullcalendar-2.1.1/lib/moment.min.js'></script>
+<script src='http://fullcalendar.io/js/fullcalendar-2.1.1/fullcalendar.min.js'></script>
+
+
+<!--
+<script src='assets/fullcalendarmaster/js/jquery-1.10.2.js' type="text/javascript"></script>
+<script src='assets/fullcalendarmaster/js/fullcalendar.js' type="text/javascript"></script>
+<script src='assets/fullcalendarmaster/js/jquery-ui.custom.min.js' type="text/javascript"></script>
+-->
+<script>
 	$(document).ready(function() {
-	    var date = new Date();
+		var date = new Date();
 		var d = date.getDate();
 		var m = date.getMonth();
 		var y = date.getFullYear();
@@ -40,8 +61,8 @@ include('navbar.php');
 			// make the event draggable using jQuery UI
 			$(this).draggable({
 				zIndex: 999,
-				revert: true,      // will cause the event to go back to its
-				revertDuration: 0  //  original position after the drag
+				revert: true, // will cause the event to go back to its
+				revertDuration: 0 //  original position after the drag
 			});
 
 		});
@@ -50,7 +71,7 @@ include('navbar.php');
 		/* initialize the calendar
 		-----------------------------------------------------------------*/
 
-		var calendar =  $('#calendar').fullCalendar({
+		var calendar = $('#calendar').fullCalendar({
 			header: {
 				left: 'title',
 				center: 'agendaDay,agendaWeek,month',
@@ -63,23 +84,22 @@ include('navbar.php');
 
 			axisFormat: 'h:mm',
 			columnFormat: {
-                month: 'ddd',    // Mon
-                week: 'ddd d', // Mon 7
-                day: 'dddd M/d',  // Monday 9/7
-                agendaDay: 'dddd d'
-            },
-            titleFormat: {
-                month: 'MMMM yyyy', // September 2009
-                week: "MMMM yyyy", // September 2009
-                day: 'MMMM yyyy'                  // Tuesday, Sep 8, 2009
-            },
+				month: 'ddd', // Mon
+				week: 'ddd d', // Mon 7
+				day: 'dddd M/d', // Monday 9/7
+				agendaDay: 'dddd d'
+			},
+			titleFormat: {
+				month: 'MMMM yyyy', // September 2009
+				week: "MMMM yyyy", // September 2009
+				day: 'MMMM yyyy' // Tuesday, Sep 8, 2009
+			},
 			allDaySlot: false,
 			selectHelper: true,
 			select: function(start, end, allDay) {
 				var title = prompt('Event Title:');
 				if (title) {
-					calendar.fullCalendar('renderEvent',
-						{
+					calendar.fullCalendar('renderEvent', {
 							title: title,
 							start: start,
 							end: end,
@@ -115,22 +135,21 @@ include('navbar.php');
 
 			},
 
-			events: [
-				{
+			events: [{
 					title: 'All Day Event',
 					start: new Date(y, m, 1)
 				},
 				{
 					id: 999,
 					title: 'Repeating Event',
-					start: new Date(y, m, d-3, 16, 0),
+					start: new Date(y, m, d - 3, 16, 0),
 					allDay: false,
 					className: 'info'
 				},
 				{
 					id: 999,
 					title: 'Repeating Event',
-					start: new Date(y, m, d+4, 16, 0),
+					start: new Date(y, m, d + 4, 16, 0),
 					allDay: false,
 					className: 'info'
 				},
@@ -149,8 +168,8 @@ include('navbar.php');
 				},
 				{
 					title: 'Birthday Party',
-					start: new Date(y, m, d+1, 19, 0),
-					end: new Date(y, m, d+1, 22, 30),
+					start: new Date(y, m, d + 1, 19, 0),
+					end: new Date(y, m, d + 1, 22, 30),
 					allDay: false,
 				},
 				{
@@ -165,77 +184,66 @@ include('navbar.php');
 
 
 	});
-
 </script>
 <style>
-
 	body {
 		margin-top: 40px;
 		text-align: center;
 		font-size: 14px;
-		font-family: "Helvetica Nueue",Arial,Verdana,sans-serif;
+		font-family: "Helvetica Nueue", Arial, Verdana, sans-serif;
 		background-color: #DDDDDD;
-		}
+	}
 
 	#wrap {
 		width: 1100px;
 		margin: 0 auto;
-		}
+	}
 
 	#external-events {
 		float: left;
 		width: 150px;
 		padding: 0 10px;
 		text-align: left;
-		}
+	}
 
 	#external-events h4 {
 		font-size: 16px;
 		margin-top: 0;
 		padding-top: 1em;
-		}
+	}
 
-	.external-event { /* try to mimick the look of a real event */
+	.external-event {
+		/* try to mimick the look of a real event */
 		margin: 10px 0;
 		padding: 2px 4px;
 		background: #3366CC;
 		color: #fff;
 		font-size: .85em;
 		cursor: pointer;
-		}
+	}
 
 	#external-events p {
 		margin: 1.5em 0;
 		font-size: 11px;
 		color: #666;
-		}
+	}
 
 	#external-events p input {
 		margin: 0;
 		vertical-align: middle;
-		}
+	}
 
 	#calendar {
-/* 		float: right; */
-        margin: 0 auto;
+		/* 		float: right; */
+		margin: 0 auto;
 		width: 900px;
 		background-color: #FFFFFF;
-		  border-radius: 6px;
-        box-shadow: 0 1px 2px #C3C3C3;
-		}
-
+		border-radius: 6px;
+		box-shadow: 0 1px 2px #C3C3C3;
+	}
 </style>
-</head>
-<body>
-<div id='wrap'>
-
-<div id='calendar'></div>
-
-<div style='clear:both'></div>
-</div>
-
 
 <?php
-include('sidebar.php');
+
 include('bottom.php');
 ?>
